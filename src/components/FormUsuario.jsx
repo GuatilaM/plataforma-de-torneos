e925c35f1bcb2dc3d760eb447703db8f3aedc0c1
guatilaM <guatilam@gmail.com>
2024-10-30 23:35:23 -0500

    feat: implementacion Formik con validacion Yup
    Primera implementacion Formik para login. Contiene campos para email y contraseña. Solo front-end

---
+++ src/components/FormUsuario.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
+
+function FormUsuario() {
+    return (
+        <Formik
+            initialValues={{email: '', password: ''}}
+            validationSchema={
+                Yup.object({
+                    email: Yup.string().email('Email inválido').required('Campo requerido'),
+                    password: Yup.string().required('Campo Requerido').min(4, 'La contraseña debe tener al menos 4 caracteres')
+                })
+            }
+            onSubmit={(values, {setSubmitting}) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            <Form>
+                <label htmlFor="email">Email</label>
+                <Field name="email" type="text" />
+                <ErrorMessage name="email" />
+                
+                <label htmlFor="password">Contraseña</label>
+                <Field name="password" type="password" />
+                <ErrorMessage name="password" />
+
+                <button type="submit">Enviar</button>
+            </Form>
+        </Formik>
+    );
+}
+
+export default FormUsuario;
