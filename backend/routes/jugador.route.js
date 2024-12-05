383671f9c03d2141620d3bc839696579b5abc286
guatilaM <guatilam@gmail.com>
2024-12-04 22:46:06 -0500

    feat: backend - añade rutas Update y Delete para Jugador

--- backend/routes/jugador.route.js
+++ backend/routes/jugador.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Jugador = require('../models/Jugador');
 
 //CREATE Jugador
-router.post('/crear', async(req, res, next) => {
+router.post('/crear', async (req, res, next) => {
     try {
         const data = await Jugador.create(req.body);
         console.log(data);
@@ -14,10 +14,46 @@ router.post('/crear', async(req, res, next) => {
 });
 
 //READ Jugador
-router.get('/', async(req, res, next) => {
+router.get('/', async (req, res, next) => {
     try {
-         const data = await Jugador.find();
-         res.json(data);
+        const data = await Jugador.find();
+        res.json(data);
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// UPDATE Jugador
+router.route('/editar/:id')
+    .get(async(req, res, next) => {
+        try {
+            const data = await Jugador.findById(req.params.id);
+            res.json(data);
+        } catch (error) {
+            return next(error);
+        }
+    })
+    .put(async(req,res,next) => {
+        try {
+            const data = await Jugador.findByIdAndUpdate(
+                req.params.id,
+                { $set: req.body },
+                { new: true }
+            );
+            console.log(data);
+            res.json(data);
+        } catch (error) {
+            return next(error);
+        }
+    });
+
+// DELETE Jugador
+router.delete('/eliminar/:id', async(req, res, next) => {
+    try {
+        const data = await Jugador.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            msg: data,
+        });
     } catch (error) {
         return next(error);
     }
