96ea802a52d8cab4264677bf8af1b051f1dd5191
guatilaM <guatilam@gmail.com>
2025-01-06 20:56:28 -0500

    fix: en modo móvil arregla alineación y funcionamiento de dropdowns en navbar

--- src/components/BarraNav.jsx
+++ src/components/BarraNav.jsx
@@ -6,50 +6,54 @@ import { Link } from 'react-router-dom';
 
 function BarraNav() {
     return(
-        <Navbar expand="lg" className='bg-body-tertiary'> 
+        <Navbar collapseOnSelect expand="lg" className='bg-body-tertiary'> 
             <Container>
                 <Navbar.Brand>
                     <Link to={'/'} className='nav-link'>
                         Plataforma de torneos
                     </Link>
                 </Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse className='justify-content-end'>
-                    <NavDropdown title="Crear">
-                        <NavDropdown.Item as='div'>
-                            <Link to={'torneos/crear'} className='nav-link'>
-                                Torneo
-                            </Link>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as='div'>
-                            <Link to={'jugadores/crear'} className='nav-link'>
-                                Jugador
-                            </Link>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as='div'>
-                            <Link to={'equipos/crear'} className='nav-link'>
-                                Equipo
-                            </Link>
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown title="Ver" className='mx-3'>
-                        <NavDropdown.Item as='div'>
-                            <Link to={'/'} className='nav-link'>
-                                Torneos
-                            </Link>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as='div'>
-                            <Link to={'jugadores/ver'} className='nav-link'>
-                                Jugadores
-                            </Link>
-                        </NavDropdown.Item>
-                        <NavDropdown.Item as='div'>
-                            <Link to={'equipos/ver'} className='nav-link'>
-                                Equipos
-                            </Link>
-                        </NavDropdown.Item>
-                    </NavDropdown>
-                </Navbar.Collapse>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+                <Nav> {/* justifica los dropdowns al final 'justify-content-end' */}
+                    <Navbar.Collapse>
+                        <Nav className='me-auto'>
+                            <NavDropdown title="Crear">
+                                <NavDropdown.Item as='div'>
+                                    <Link to={'torneos/crear'} className='nav-link'>
+                                        Torneo
+                                    </Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Item as='div'>
+                                    <Link to={'jugadores/crear'} className='nav-link'>
+                                        Jugador
+                                    </Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Item as='div'>
+                                    <Link to={'equipos/crear'} className='nav-link'>
+                                        Equipo
+                                    </Link>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Ver">
+                                <NavDropdown.Item as='div'>
+                                    <Link to={'/'} className='nav-link'>
+                                        Torneos
+                                    </Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Item as='div'>
+                                    <Link to={'jugadores/ver'} className='nav-link'>
+                                        Jugadores
+                                    </Link>
+                                </NavDropdown.Item>
+                                <NavDropdown.Item as='div'>
+                                    <Link to={'equipos/ver'} className='nav-link'>
+                                        Equipos
+                                    </Link>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Nav>
             </Container>
         </Navbar>
     );
